3e6a7b586080904158e39c674dbfd70313a1e93e
Angel Geovanny Hernández Alegría <aghabizzle@outlook.com>
2022-05-14 15:22:51 -0500

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <script></script>
 <template>
-    
+    <h1>Work with an Amazing tool</h1>
+    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet ex consectetur, tincidunt purus sed, auctor dui. Maecenas vitae tellus nec massa convallis eleifend. Aenean vel consequat nulla. Morbi vestibulum dolor ut venenatis tristique. </h3>
 </template>
 
 <style>
