4e87defb5e6b64e88a4b12f9b1445ff2f682544f
Angel Geovanny Hernández Alegría <aghabizzle@outlook.com>
2022-05-14 16:07:59 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import Header from './components/Header.vue'
 
 <style>
 .Header{
-  background-image: url(https://www.canva.com/design/DAFAsXGccmo/b3OzHUQ6EB8FgyepdA1rFQ/view?utm_content=DAFAsXGccmo&utm_campaign=share_your_design&utm_medium=link&utm_source=shareyourdesignpanel);
-  
+  background-image: url(../src/assets/Logos/Portada.svg);
+  background-position: 30%;
 }
 </style>
