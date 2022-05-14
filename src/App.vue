682f799bfd14860f1f793a1bce2a3a55c0fbdaf8
Angel Geovanny Hernández Alegría <aghabizzle@outlook.com>
2022-05-14 15:22:48 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import NavBar from './components/NavBar.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>  
 <div>
   <NavBar />
-  
+  <Header />
 </div>
   
   
