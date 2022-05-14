2978910a78c53260386806229eed1da1ce4382e5
Angel Geovanny Hernández Alegría <aghabizzle@outlook.com>
2022-05-14 15:45:46 -0500

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,38 @@
 <script></script>
 <template>
-    <h1>Work with an Amazing tool</h1>
-    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet ex consectetur, tincidunt purus sed, auctor dui. Maecenas vitae tellus nec massa convallis eleifend. Aenean vel consequat nulla. Morbi vestibulum dolor ut venenatis tristique. </h3>
+<div class="ContainerHeadline">
+    <header>Work with an Amazing tool</header>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet ex consectetur, tincidunt purus sed, auctor dui. Maecenas vitae tellus nec massa convallis eleifend. Aenean vel consequat nulla. Morbi vestibulum dolor ut venenatis tristique. </p>
+    <a href="#">Firmar</a>
+    <br><br>
+    <span>Find us on</span>
+    <ul>
+        <li>FacebookIcon</li>
+        <li>Instagramicon</li>
+        <li>TwitterIcon</li>
+        <li>GoogleIcon</li>
+    </ul>
+</div>
+   
 </template>
 
 <style>
+.ContainerHeadline{
+    display: grid;
+    place-items: center;
+}
+header{
+    font: bolder;
+    font-size: x-large;
+}
+p{
+    font-size: medium;
+    padding: 0px 50px 0px 50px ;
+
+}
+p::first-letter{
+    font-size: larger;
+}
+
 
 </style>
