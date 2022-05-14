e1fc1616729e09c4f42d28e72e65fceeec85775b
Angel Geovanny Hernández Alegría <aghabizzle@outlook.com>
2022-05-14 13:30:31 -0500

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,14 +10,15 @@
                 <a class="link" href="#">MAPA</a>
             </li>
             <li class="parent">
+                <a class="link" href="#">About Us</a>
+            </li>
+            <li class="parent">
                 <a class="link" href="#">Firmas</a>
             </li>
             <li class="parent">
                 <a class="link" href="#">Donaciones</a>
             </li>
-            <li class="parent">
-                <a class="link" href="#">About Us</a>
-            </li>
+            
         </ul>
     </nav>
 </template>
