cc29cd0c4aeb6e4b132d9d452814b2d8f09305bf
Angel Geovanny Hernández Alegría <aghabizzle@outlook.com>
2022-05-14 13:17:12 -0500

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,12 @@
 <script></script>
 
 <template>
-    
+    <nav>
+        <div class="ContainerLogo">
+            <span>Axojonautas</span>
+            
+        </div>
+    </nav>
 </template>
 
 <style>
